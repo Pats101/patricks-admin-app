563722f9f3e234043fa80d79aa43b8901af548ef
Patrick Tonderai Ganhiwa <ptganhiwa8@gmail.com>
2022-04-01 22:58:39 +0200

    worked on the widgets on dashboard

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import FeaturedInfo from '../../components/featured-info/FeaturedInfo';
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className='home'>
+      <FeaturedInfo />
+    </div>
+  )
+}
