1df7b8a9439b827b8d70d60b4123d52a346ec344
Patrick Tonderai Ganhiwa <ptganhiwa8@gmail.com>
2022-04-01 21:55:49 +0200

    worked on the sidebar menu sections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">Other pages</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar/quick-menu/SidebarMenu.jsx
@@ -0,0 +1,30 @@
+import { PersonOutlined, Storefront, TrendingUp, Assessment } from '@material-ui/icons';
+import React from 'react'
+import "./SidebarMenu.css";
+
+ export default function 
+() {
+  return (
+    <div className='sidebarMenu'>
+        <h3 className="sidebarMenuTitle">Quick Menu</h3>
+        <ul className="sidebarMenuList">
+            <li className="sidebarMenuListItem active">
+                <PersonOutlined className="sidebarMenuIcon" />
+                Users
+            </li>
+            <li className="sidebarMenuListItem">
+                <Storefront className="sidebarMenuIcon" />
+                Products
+            </li>
+            <li className="sidebarMenuListItem">
+                <TrendingUp className="sidebarMenuIcon" />
+                Transactions
+            </li>
+            <li className="sidebarMenuListItem">
+                <Assessment className="sidebarMenuIcon" />
+                Reports
+            </li>            
+        </ul>
+    </div>
+  )
+}
